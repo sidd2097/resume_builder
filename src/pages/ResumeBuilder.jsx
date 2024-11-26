2b8f998864e21db5a86a2d9465228b7de9fef553
Sidd <siddheshposam20@gmail.com>
2024-11-26 16:22:06 +0530

    Added other forms

--- src/pages/ResumeBuilder.jsx
+++ src/pages/ResumeBuilder.jsx
@@ -1,6 +1,9 @@
 import React, { useContext, useRef } from 'react';
 import { ResumeContext } from '../context/ResumeContext';
 import PersonalInfoForm from '../components/Form/PersonalInfoForm';
+import ProjectsForm from '../components/Form/ProjectsForm';
+import SkillsForm from '../components/Form/ProjectsForm';
+import WorkExperienceForm from '../components/Form/WorkExperienceForm';
 import LayoutOne from '../components/ResumePreview/LayoutOne';
 import { useReactToPrint } from 'react-to-print';
 
@@ -16,6 +19,9 @@ const ResumeBuilder = () => {
     <div className="grid grid-cols-2 gap-4 p-6">
       <div>
         <PersonalInfoForm />
+        <ProjectsForm/>
+        <SkillsForm/>
+        <WorkExperienceForm/>
         {/* Include other form components */}
         <button onClick={handlePrint} className="mt-4 px-4 py-2 bg-blue-600 text-white">
           Save as PDF
